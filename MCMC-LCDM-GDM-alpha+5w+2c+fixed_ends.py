7612dc41204e96bb55453ce6778dbf0a5c450a53
mcmeiers <mcmeiers@gmail.com>
2022-05-31 14:45:25 -0700

    Updated 1+5+2 MCMC

--- MCMC-LCDM-GDM-alpha+5w+2c+fixed_ends.py
+++ MCMC-LCDM-GDM-alpha+5w+2c+fixed_ends.py
@@ -1,16 +1,17 @@
 # %%
 from pathlib import Path
+import numpy as np
 
 import src.gdmtools as gdmtools
 
 from cobaya.run import run
 from cobaya.log import LoggedError
 
+
 from mpi4py import MPI
 
 # %% Define project name and output directory
 PROJECT_NAME = "gdm_alpha_5w_2c_fixed_ends"
-import os
 
 PROJECT_DIR = Path("/opt/project/") / PROJECT_NAME / ""
 
@@ -18,39 +19,16 @@ CHAIN_DIR = PROJECT_DIR / "chains/"
 (CHAIN_DIR / "").mkdir(exist_ok=True, parents=True)
 
 COBAYA_PACKAGES_PATH = Path("/software/cobaya_packages")
-CLASS_PATH = COBAYA_PACKAGES_PATH / "code/class_public-designer/"
 
-# %% create wModel
+# %% laod gdm model
 
-log10a_epoch_intervals_endpoints = (-14.0, -4.5, -2.5, 0.0)
-n_interval_knots = (0, 4, 1)
+gdm_model = gdmtools.yaml.load(PROJECT_DIR / f"{PROJECT_NAME}+model.yaml")
 
-log10a_knots = gdmtools.make_log10a_knots_from_epochs(
-    log10a_epoch_intervals_endpoints, n_interval_knots
-)[:-1]
-fixed_points = ((-14, -1), (0, 1))
-
-w_range_filter = (
-    (-14, {"min": -1, "max": 1 / 3.0}),
-    (-7, {"min": -1, "max": 1}),
-    (-2, {"min": 1 / 3, "max": 1}),
-)
-
-w_model = gdmtools.wModel(log10a_knots, fixed_points, range_filter=w_range_filter)
-
-# %% create gdm model
-gdm_model = gdmtools.gdmModel(
-    w_model,
-    alpha={"prior": {"min": 0, "max": 0.1}},
-    c_eff2={"prior": {"min": 0, "max": 1}},
-    c_vis2={"prior": {"min": 0, "max": 1}},
-    z_alpha=3000,
-)
 # %% lcdm model params
 
 lcdm_params = {
     "logA": {
-        "prior": {"min": 2.98, "max": 3.1},
+        "prior": {"min": 1.61, "max": 3.91},
         "ref": {"dist": "norm", "loc": 3.05, "scale": 0.001},
         "proposal": 0.001,
         "latex": "\\log(10^{10} A_\\mathrm{s})",
@@ -58,13 +36,13 @@ lcdm_params = {
     },
     "A_s": {"value": "lambda logA: 1e-10*np.exp(logA)", "latex": "A_\\mathrm{s}"},
     "n_s": {
-        "prior": {"min": 0.94, "max": 1.0},
+        "prior": {"min": 0.8, "max": 1.2},
         "ref": {"dist": "norm", "loc": 0.965, "scale": 0.004},
         "proposal": 0.002,
         "latex": "n_\\mathrm{s}",
     },
     "theta_s_1e2": {
-        "prior": {"min": 1.03, "max": 1.05},
+        "prior": {"min": 0.5, "max": 10},
         "ref": {"dist": "norm", "loc": 1.0416, "scale": 0.0004},
         "proposal": 0.0002,
         "latex": "100\\theta_\\mathrm{s}",
@@ -74,13 +52,13 @@ lcdm_params = {
     "H0": {"min": 60, "max": 80, "latex": "H_0"},
     "sigma8": {"latex": "\sigma_8"},
     "omega_b": {
-        "prior": {"min": 0.02, "max": 0.025},
+        "prior": {"min": 0.005, "max": 0.1},
         "ref": {"dist": "norm", "loc": 0.0224, "scale": 0.0001},
         "proposal": 0.0001,
         "latex": "\\Omega_\\mathrm{b} h^2",
     },
     "omega_cdm": {
-        "prior": {"min": 0.11, "max": 0.13},
+        "prior": {"min": 0.001, "max": 0.99},
         "ref": {"dist": "norm", "loc": 0.12, "scale": 0.001},
         "proposal": 0.0005,
         "latex": "\\Omega_\\mathrm{c} h^2",
@@ -91,10 +69,19 @@ lcdm_params = {
         "proposal": 0.4,
     },
     "tau_reio": {"latex": "\\tau_\\mathrm{reio}"},
+    "Omega_gdm_max": None,
+    "z_gdm_max": None
 }
 
-# %%
+# %% setup gdm_likelihood, it has a trivial likelihood but Omega_gdm_max and z_gdm_max are computed here
 
+def gdm_likelihood(_self):
+    bg =_self.provider.get_CLASS_background()
+    Omega_gdm = bg['(.)rho_gdm']/bg['H [1/Mpc]']**2
+    Omega_gdm_max_idx = np.argmax(Omega_gdm)
+    return (0, {'Omega_gdm_max': Omega_gdm[Omega_gdm_max_idx], 'z_gdm_max': bg['z'][Omega_gdm_max_idx]})
+
+# %%
 
 cobaya_info = dict(
     theory={
@@ -104,7 +91,8 @@ cobaya_info = dict(
                 "non_linear": "hmcode",
                 "l_max_scalars": 5000,
             },
-            "path": str(CLASS_PATH),
+            #"path": str(CLASS_PATH),
+            #'provide': {'get_CLASS_background': None},
         }
     },
     params={**lcdm_params, **gdm_model.params},
@@ -113,6 +101,9 @@ cobaya_info = dict(
         "planck_2018_lowl.EE": None,
         "planck_2018_highl_plik.TTTEEE_lite": None,
         "planck_2018_lensing.clik": None,
+        'gdm': {'external': gdm_likeihood,
+                'output_params': ['Omega_gdm_max','z_gdm_max'],
+                'requires': {'CLASS_background': None}},
     },
     sampler=dict(
         mcmc={
@@ -124,7 +115,8 @@ cobaya_info = dict(
             "Rminus1_cl_stop": 0.025,
         }
     ),
-    output=str(CHAIN_DIR),
+    output=str(CHAIN_DIR / PROJECT_NAME),
+    resume= True,
     packages_path=str(COBAYA_PACKAGES_PATH),
 )
 
