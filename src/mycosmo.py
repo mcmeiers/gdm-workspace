573a67e4e0c51784b12db58b18a25939c02b8aae
Michael Meiers <mcmeiers@gmail.com>
2022-07-13 15:42:51 -0700

    Sytem sync

--- src/mycosmo.py
+++ src/mycosmo.py
@@ -7,7 +7,164 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
+from pathlib import Path
+import os
+
+from pyrsistent import m
+from functools import reduce
+
 # %% ploting utilities
+
+
+PACKAGE_DIR = Path(os.path.join(os.path.dirname(__file__)))
+DATA_DIR = Path.resolve(PACKAGE_DIR / "../data/")
+
+
+def plot_Cls_against_planck18(
+    cls_of_mdl, ell_min=2, ell_max=2508, modes=("tt", "te", "ee"), fig=None
+):
+    if fig is None:
+        fig, axs = plt.subplots(len(modes), sharex=True, figsize=(14, 8))
+        fig.subplots_adjust(hspace=0)
+    else:
+        axs = fig.gca()
+
+    if len(modes) == 1:
+        axs = [axs]
+
+    # The data file columns give Dℓ = ℓ(ℓ+1)Cℓ / 2π in units of μK2, and the lower and upper 68% uncertainties.
+
+    assert len(modes) == len(axs)
+    assert ell_min < ell_max
+
+    for mode, ax in zip(modes, axs):
+        data = np.loadtxt(
+            DATA_DIR / "planck" / f"COM_PowerSpect_CMB-{mode.upper()}-full_R3.01.txt"
+        )
+        ell_mask = (data[:, 0] >= ell_min) & (data[:, 0] <= ell_max)
+        ax.errorbar(data[ell_mask, 0], data[ell_mask, 1], yerr=data[ell_mask, 2:].T)
+
+    labels = []
+    for mdl, cls in cls_of_mdl.items():
+        ell_mask = (cls["ell"] >= ell_min) & (cls["ell"] <= ell_max)
+        ells = cls["ell"][ell_mask]
+        labels.append(mdl)
+        for mode, ax in zip(modes, axs):
+            ax.plot(ells, cls[mode][ell_mask], label=mdl)
+            ax.set_ylabel(
+                r"$\ell(\ell+1)/(2\pi)\,C^{" + str(mode) + "}_\ell\;(\mu \mathrm{K}^2)$"
+            )
+
+    plt.xlabel(r"$\ell$")
+    fig.legend(labels, loc="right")
+
+    if len(modes) == 1:
+        axs = axs[0]
+
+    return fig
+
+
+def plot_residuals_Cls_from_planck18(
+    cls_of_mdl,
+    ell_min=2,
+    ell_max=2508,
+    modes=("tt", "te", "ee"),
+    fig=None,
+):
+
+    if fig is None:
+        fig, axs = plt.subplots(len(modes), sharex=True, figsize=(14, 8))
+        fig.subplots_adjust(hspace=0)
+    else:
+        axs = fig.gca()
+
+    if len(modes) == 1:
+        axs = [axs]
+
+    assert len(modes) == len(axs)
+    assert ell_min < ell_max
+
+    ref_ells = {}
+    ref_cl = {}
+    var_cls = {}
+    for mode in modes:
+        data = np.loadtxt(
+            DATA_DIR / "planck" / f"COM_PowerSpect_CMB-{mode.upper()}-full_R3.01.txt"
+        )
+        ref_ells[mode] = data[:, 0]
+        ref_cl[mode] = data[:, 1]
+        var_cls[f'-{mode}'] = data[:, 2]
+        var_cls[f'+{mode}'] = data[:, 3]
+    
+    res_factor = {}
+    ell_masks = {}
+    for mode in modes:
+        if mode[0]==mode[1]:
+            ell_mask = (ref_ells[mode] >= ell_min) & (ref_ells[mode] <= ell_max)
+            ell_masks[mode] = ell_mask
+            ref_ells[mode] = ref_ells[mode][ell_mask]
+            ref_cl[mode] = ref_cl[mode][ell_mask]
+            var_cls[f'-{mode}'] = var_cls[f'-{mode}'][ell_mask]
+            var_cls[f'+{mode}'] = var_cls[f'+{mode}'][ell_mask] 
+            res_factor[mode]= 1/ref_cl[mode]
+        else:
+            common_ell = reduce(np.intersect1d,(ref_ells[mode],ref_ells[mode[0]+ mode[0]],ref_ells[mode[1]+ mode[1]]))
+            common_ell = common_ell[(common_ell >= ell_min) & (common_ell <= ell_max)]
+            ref_ells[mode], ell_masks[mode], _ = np.intersect1d(ref_ells[mode],common_ell,return_indices=True)
+            ell_mask1 = np.in1d(ref_ells[mode[0]+ mode[0]],common_ell)
+            ell_mask2 = np.in1d(ref_ells[mode[1]+ mode[1]],common_ell)
+            res_factor[mode]= 1/(ref_cl[mode[0]+ mode[0]][ell_mask1] + ref_cl[mode[1]+ mode[1]][ell_mask2])**(1/2.)
+            var_cls[f'-{mode}'] = var_cls[f'-{mode}'][ell_masks[mode]]
+            var_cls[f'+{mode}'] = var_cls[f'+{mode}'][ell_masks[mode]] 
+        
+    ax in zip(modes, axs):
+        ax.fill_between(data[ell_mask, 0], data[ell_mask, 2], data[ell_mask, 3])
+        
+
+        ref_ells[mode] = data[ell_mask, 0]
+        ref_cl[mode] = data[ell_mask, 1]
+        ax.fill_between(data[ell_mask, 0], data[ell_mask, 2], data[ell_mask, 3])
+
+
+            
+    
+    labels = []
+    for mdl, cls in cls_of_mdl.items():
+        ell_intersection_info={mode:np.intersect1d(
+                ref_ells[mode], cls["ell"], return_indices=True
+            ) for mode in modes}
+        for mode, ax in zip(modes, axs):
+            common_ells,ref_ind,mdl_ind = ell_intersection_info[mode]
+            scale_ells, scale_ell_ind0,scale_ell_ind1 = ell_intersection_info(np.intersect1d(ref_ells[mode[0] + mode[0]],ref_ells[mode[1] + mode[1]], return_indices=True)
+            )
+            ax.plot(
+                common_ells,
+                (cls[mode][mdl_ind] - ref_cl[mode][ref_ind])
+                / (
+                    (
+                        (ref_cl[mode[0] + mode[0]][scale_ell_ind0] * ref_cl[mode[1] + mode[1]][scale_ell_ind1])
+                        ** (1 / 2.0)
+                    )[ref_ind]
+                ),
+                label=mdl,
+            )
+
+    for mode, ax in zip(modes, axs):
+        ax.set_ylabel(
+            r"$\Delta\,C^{"
+            + str(mode)
+            + "}_\ell/\,C^{"
+            + str(mode)
+            + "}_\ell\;(\mu \mathrm{K}^2)$"
+        )
+
+    plt.xlabel(r"$\ell$")
+    fig.legend(labels, loc="right")
+
+    if len(modes) == 1:
+        axs = axs[0]
+
+    return fig
 
 
 def plot_Cls(cls_of_mdl, ell_min=2, ell_max=2508, modes=("tt", "te", "ee"), fig=None):
